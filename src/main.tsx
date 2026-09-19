8680a2b0f417d5e3a28656def28562f748aacae5
Antonio Silva <124893465+tombemol@users.noreply.github.com>
2026-09-19 05:47:29 -0300

    feat(android): rotear runtime móvel para interface dedicada

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import AndroidApp from "./AndroidApp";
 import "./styles.css";
+
+const isAndroid = /android/i.test(navigator.userAgent);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {isAndroid ? <AndroidApp /> : <App />}
   </StrictMode>
 );
