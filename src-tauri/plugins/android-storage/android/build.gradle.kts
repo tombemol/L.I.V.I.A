207f42868f7fc640e45b201078d59a5715b6e689
Antonio Silva <124893465+tombemol@users.noreply.github.com>
2026-09-19 07:27:12 -0300

    feat(android): add storage bridge (src-tauri/plugins/android-storage/android/build.gradle.kts)

---
+++ src-tauri/plugins/android-storage/android/build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.tombemol.livia.storage"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_1_8
+    }
+}
+
+dependencies {
+    implementation(project(":tauri-android"))
+}
